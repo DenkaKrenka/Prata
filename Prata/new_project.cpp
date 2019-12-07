7376486ceab33b1adbfa9787506e2c422fcec226
DenkaKrenka <nn.ru@inbox.ru>
2019-12-08 01:12:40 +0300

    release v1

---
+++ Prata/new_project.cpp
@@ -0,0 +1,143 @@
+/*
+#include <io stream>
+using namespace std;
+int main()
+{
+cout <<"Daniil Kostyunichev"<<endl;
+system("pause");
+return 0;
+*/
+
+/*
+#include <iostream>
+'using namespace std;
+int convent(int furlong) {
+	int furlong = yard = 220
+		return furlong;
+}
+itn main()t
+int tmp = convent(5);
+cout << tmp
+<< end / ,
+return 0;
+}
+*/
+
+/*
+#include <io stream>
+void func_1(void);
+void func_2(void);
+int main()
+{
+	func_1();
+	func_1();
+	func_2();
+	func_2();
+	return 0;
+}
+void func_1()
+{
+	using std::cout;
+	using std::endl;
+	cout << "Three blind mice" << endl;
+}
+void func_2()
+{
+	using std::cout;
+	using std::endl;
+	cout << "See how they run" << endl;
+}
+*/
+
+/*
+#include <io stream>
+int main()
+{
+	using namespace std;
+	const int Fut_1 = 12;
+	cout << "180\d";
+	unsigned shortsuim;
+	cin >> duim;
+	int Fut_R, duim_R;
+	Fut_R = duim / Fut_1;
+	duim_R = duim%Fut_1;
+	cout << "180:" << fut_R << "футов и"
+		<< duim_R << "дюймов." << endl;
+	cin.get();
+	cn, get();
+	return0;
+}
+*/
+
+/*
+#include <io stream>
+using namespace std;
+int main()
+{
+	const float mile_km = 1.609;
+	const float gl_litr = 3.875;
+	float litr_100, km, mile;
+	float mile_gl;
+	setlocale(0, "");
+	cout << "Please enter cels:";
+	cin >> litr_100;
+	km = 100 / litr_100;
+	mile = km / mile_km;
+	mile_gl = (mile*gl_litr);
+	cout << "Итого" << mile_gl << "миль на галлон." << endl;
+	return 0;
+}
+*/
+
+/*
+#include  "stdafx.h"
+#include <io stream>;
+using namespace std;
+const double d = 0.0254;
+const double f = 12;
+const double pd = 2.2;
+int main()
+{
+	double ftheight, dheight, mass;
+	cout << "please enter your height (ft) here:";
+	cin >> ftheight;
+	cout << "please enter your height (d) here:";
+	cin >> dtheight;
+	cout << "please enter your height (pb) here:";
+	cin >> mass;
+	mass = mass / pb;//pb in kg
+	ftheight = ftheight*f;//ft in d
+	dheight = (dheight + ftheigt)*d;
+	double BMI = mass / (dheight*dheight);
+	cout << BMI << endl;
+	return 0;
+}
+*/
+
+/*
+#include "stadafx.h"
+#include <io stream>
+using namespace std;
+int main()
+{
+	double cels, fahr;
+	cout << "Please enter a celsius values:";
+	cin >> cels;
+	cout << cels << degrees Celsius is"<<(cels*1.8+32.0)<<"degrees Fahrenheit\n";
+		return0;
+}
+*/
+
+/*
+#include"stdafx.h"
+#include<io stream>
+using namespace std;
+int main()
+{
+	double cels, fahr;
+	cout << "Please enter celsius vslue:";
+	cin >> cels;
+	cout << cels << "degrees Celsius is" << (cels*1.8 + 32.8) << "degrees fahrenhe it\n";
+	return0
+}
+*/
